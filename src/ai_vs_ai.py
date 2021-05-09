3073160fcb3e6701f8e43512cc957c7dbdee6a74
AltiTorie <Arek.ste99@wp.pl>
2021-05-09 19:59:06 +0200

    Fixed AI vs AI, added new evaluator heuristic, fixed naming issues in minmax.py

--- src/ai_vs_ai.py
+++ src/ai_vs_ai.py
@@ -18,14 +18,12 @@ if __name__ == '__main__':
     moves = {Player.A: [], Player.B: []}
     depth = 6
     while not board.no_more_moves():
-
-        if player is Player.A:
-            logging.info(f'Now moving: player{player}')
-            am, board = mm.run(board, depth, True, player)
-            board.print_state()
-            if not am:
-                player = player.next()
-            moves[player].append(MinMax.moves_count)
+        logging.info(f'Now moving: player{player}')
+        am, board = mm.run(board, depth, True, player)
+        board.print_state()
+        if not am:
+            player = player.next()
+        moves[player].append(MinMax.moves_count)
         logging.info(MinMax.moves_count)
         MinMax.moves_count = 0
     board.clean_board()
